6a2ce030aa3e502c467172ce52b7fd30b5d01ede
Akash Jonnalagadda <jonnalagaddaakash777@gmail.com>
2026-07-04 13:35:26 +0530

    Set basename for BrowserRouter in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/practice" element={<Problems />} />
